6abcd8deb20e59b0ea2252b44971dab7ae9199be
Jessica Jumelles <jessicajumelles@gmail.com>
2021-09-29 00:59:47 -0400

    Week one exercise

---
+++ weekOneExercise/ExerciseOne.java
@@ -0,0 +1,14 @@
+package weekOneExercise;
+
+public class ExerciseOne {
+
+	public static void main(String[]args) {
+		double num1 = 10.5;
+		double num2 = 11.5;
+		
+		double result = num1*num2;
+		
+		System.out.print(result);
+	}
+	
+}
---
+++ weekOneExercise/ExerciseTwo.java
@@ -0,0 +1,18 @@
+package weekOneExercise;
+
+public class ExerciseTwo {
+	
+	public static void main (String[]args) {
+		
+		int x = 3;
+		
+		if(x == 3) {
+			System.out.print("x is equal to 3.");
+		}
+		else {
+			System.out.print("x is NOT equal to 3");
+		}
+		
+	}
+
+}
